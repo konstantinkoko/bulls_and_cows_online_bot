9079ad209544d4dec08e1fe457ee8b79106dcd7d
Konstantin_Kozhevnikov <kolobokkoko@gmail.com>
2023-03-24 00:28:28 +0500

    + .env + Dockerfile

---
+++ bot_config.py
@@ -0,0 +1,11 @@
+#from dotenv import load_dotenv
+import os
+
+#load_dotenv()
+TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
+
+LANGUAGE = "ru"
+#GAME_MODE = ["NUMBER", "WORD"]
+
+# GAME SETTINGS:
+NUMBER_LENGHT = 4
